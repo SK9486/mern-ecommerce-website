bff5ee55c1c06403eafd8072525286018ba4a574
SK9486 <valli74appan@gmail.com>
2024-12-21 21:28:39 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,13 +1,15 @@
 const mongoose = require('mongoose');
-require('dotenv').config(); // Ensure dotenv is loaded
+require('dotenv').config();
 
 const dbURI = process.env.MONGO_URI;
 
-console.log("Mongo URI:", dbURI); // Debugging line to check if URI is loaded correctly
+const connectDB = () => {
+  mongoose.connect(dbURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+    .then(() => console.log("Connected to MongoDB"))
+    .catch(err => console.error("Error connecting to MongoDB:", err));
+};
 
-mongoose.connect(dbURI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-})
-  .then(() => console.log("Connected to MongoDB"))
-  .catch(err => console.error("Error connecting to MongoDB:", err));
+module.exports = { connectDB };  // CommonJS export
